c6eb87250938f349549fb5033b67a6ca8e32135c
MaxDorris <maxdorris5@gmail.com>
2023-03-08 01:01:03 -0600

    adding map modification

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -28,6 +28,23 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     // mapbox/satellite-v9
     // mapbox/satellite-streets-v11
 });
+
+//  Add a marker to the map for Los Angeles, California.
+// L.circle([34.0522, -118.2437], {
+//   radius: 300,
+//   color: "black",
+//   fillColor: 'lightyellow'
+// }).addTo(map);
+
+L.circleMarker([34.0522, -118.2437],{ // uses pixels instead of meters for radius
+
+radius: 30,
+color: 'red',
+fillColor: 'pink'
+
+}).addTo(map);
+
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
